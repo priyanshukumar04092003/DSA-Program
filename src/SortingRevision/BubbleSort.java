eeb6a823bd0e3cd271874cb09b9afc6fc8b6bddb
priyanshu kumar <priyanshukumar04092003@gmail.com>
2026-02-21 13:17:15 +0530

    Updated Bubble Sort logic

--- src/SortingRevision/BubbleSort.java
+++ src/SortingRevision/BubbleSort.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class BubbleSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int [] arr = {10,30,20,50,60};
+        int n= arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
